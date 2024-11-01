6e05feb246ec593d4144f441373d98f33dd687a8
Anna Olsson <annapolsson@hotmail.com>
2024-11-01 11:56:43 +0100

    .

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import "./Register.css";
+import "./register.css";
 
 const avatarOptions = [
   'https://api.multiavatar.com/seed1.svg',
